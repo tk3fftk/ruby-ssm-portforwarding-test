f2f7823ed0ad911a4a8272d2256ca6ae040fbc7a
tk3fftk <tktk.stereoman@gmail.com>
2021-11-17 09:32:31 +0900

    wait until session is connected, try to connect mysql

--- connect_to_mysql_over_ssm_with_socat.rb
+++ connect_to_mysql_over_ssm_with_socat.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "aws-sdk"
+require "mysql2"
 
 role_credentials = Aws::AssumeRoleCredentials.new(
   role_arn: ENV["AWS_ROLE_ARN"],
@@ -15,8 +16,58 @@ client = Aws::SSM::Client.new(
 )
 
 res = client.start_session({
-                             target: ENV["INSTANCE_ID"],
-                             document_name: "AWS-StartSSHSession",
-                             reason: "test",
-                           })
-print(res)
+  target: ENV["INSTANCE_ID"],
+  document_name: "AWS-StartPortForwardingSession",
+  reason: "test",
+  parameters: {
+    portNumber: ["3306"],
+    localPortNumber: ["13306"],
+  }
+})
+
+puts res
+session_id = res["session_id"]
+token = res["token_value"]
+stream_url = res["stream_url"]
+
+res = client.get_connection_status({
+  target: ENV["INSTANCE_ID"],
+})
+
+puts res
+
+status = ""
+max_attempt = 10
+count = 0
+
+while status != "Connected" && count < max_attempt
+  res = client.describe_sessions({
+    state: "Active", # required, accepts Active, History
+    max_results: 1,
+    #  next_token: token,
+    filters: [
+      {
+        key: "SessionId", # required, accepts InvokedAfter, InvokedBefore, Target, Owner, Status, SessionId
+        value: session_id, # required
+      },
+    ],
+  })
+  status = res["sessions"][0]["status"]
+  count += 1
+  sleep(0.5)
+  puts(res)
+end
+
+mysql_client = Mysql2::Client.new(
+  host: "127.0.0.1",
+  username: ENV["MYSQL_USERNAME"],
+  password: ENV["MYSQL_PASSWORD"],
+  port: 13306,
+  database: ENV["MYSQL_DATABASE"]
+)
+
+results = mysql_client.query("SELECT * FROM user")
+
+results.each do |row|
+  puts row
+end
