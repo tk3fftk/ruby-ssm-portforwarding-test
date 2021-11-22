8604dd695ca78e27d8925425e38ccbea15b7a03b
tk3fftk <tktk.stereoman@gmail.com>
2021-11-22 10:36:32 +0900

    add profile

--- connect_to_mysql_over_ssm_using_ssh_tunnel.rb
+++ connect_to_mysql_over_ssm_using_ssh_tunnel.rb
@@ -6,13 +6,51 @@ require "json"
 require "net/ssh/gateway"
 require "net/ssh/proxy/command"
 
+# write aws config file
+profile = ENV["AWS_PROFILE"] || "default"
+
+aws_config_file = File.expand_path("~/.aws/config")
+unless File.exist?(aws_config_file) then
+  File.open(aws_config_file, "w") do |f|
+    f.puts <<-EOS
+[default]
+region = ap-northeast-1
+output = json
+[profile #{profile}]
+region = ap-northeast-1
+output = json
+    EOS
+  end
+end
+
+# write aws credentials file
+aws_credentials_file = File.expand_path("~/.aws/credentials")
+unless File.exist?(aws_credentials_file) then
+  File.open(aws_credentials_file, "w") do |f|
+    f.puts <<-EOS
+[default]
+aws_access_key_id = #{ENV["AWS_ACCESS_KEY_ID"]}
+aws_secret_access_key = #{ENV["AWS_SECRET_ACCESS_KEY"]}
+[#{profile}]
+role_arn = #{ENV["AWS_ROLE_ARN"]}
+source_profile = default
+    EOS
+  end
+end
+
 step_user = "ec2-user"
 step_host = ENV["INSTANCE_ID"]
 db_host = ENV["DB_HOST"]
 key_path = ENV["KEY_PATH"] || "~/.ssh/id_rsa.pub"
 ssh_options = {
   keys: [key_path],
-  proxy: Net::SSH::Proxy::Command.new("aws ssm start-session --target %h --document-name AWS-StartSSHSession --parameters 'portNumber=%p'")
+  # with personal config/credentials
+  # proxy: Net::SSH::Proxy::Command.new("aws ssm start-session --target %h --document-name AWS-StartSSHSession --parameters 'portNumber=%p' --profile pn-playground"),
+  # with dynamic config/credentials
+  proxy: Net::SSH::Proxy::Command.new("aws ssm start-session --target %h --document-name AWS-StartSSHSession --parameters 'portNumber=%p' --profile #{ENV["AWS_PROFILE"]}"),
+  # with ruby proxy command (not working)
+  # proxy: Net::SSH::Proxy::Command.new("ruby /home/tk3fftk/git/ruby-ssm-portforwarding-test/ruby-ssm-startssh-test.rb -h '%h' -p '%p'"),
+  verbose: :info,
 }
 
 gateway = Net::SSH::Gateway.new(step_host, step_user, ssh_options)
@@ -35,3 +73,5 @@ gateway.open(db_host, 3306) do |forwared_port|
     puts row
   end
 end
+
+gateway.shutdown!
